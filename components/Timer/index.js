142ffd1211af662f67abccd8aed3c4d6815fc65e
Sean Jun <seanjun21@gmail.com>
2019-07-24 20:26:48 -0700

    creating button component

--- components/Timer/index.js
+++ components/Timer/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import Button from '../Button';
 
 const styles = StyleSheet.create({
   container: {
@@ -32,7 +33,8 @@ class Timer extends Component {
           <Text style={styles.time}>25:00</Text>
         </View>
         <View style={styles.lower}>
-          <Text>BUTTONS HERE</Text>
+          <Button iconName="play-circle" onPress={() => alert('it works!')} />
+          <Button iconName="stop-circle" onPress={() => alert('it works!')} />
         </View>
       </View>
     );
