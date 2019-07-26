95e2a2d488bce714e3d94ddb6acfe17800a77002
Sean Jun <seanjun21@gmail.com>
2019-07-26 16:07:33 -0700

    creating tomato reducer

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import Timer from './components/Timer';
+import reducer from './reducer';
+
+const store = createStore(reducer);
+
+console.log(store);
 
 export default function App() {
-  return <Timer />;
+  return (
+    <Provider store={store}>
+      <Timer />
+    </Provider>
+  );
 }
